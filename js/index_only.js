c9e4ee2b3e9e3b003e7cdf08b619e8bb89c25bb2
Kipper Eyderoe <k932693932@Outlook.com>
2023-06-07 10:29:14 +0800

    Uniform

--- js/index_only.js
+++ js/index_only.js
@@ -1,26 +1,17 @@
 function showTime() {
     let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
+    let hours = date.getUTCHours();
+    let minutes = date.getUTCMinutes();
+    let seconds = date.getUTCSeconds();
     let year = date.getFullYear();
     let month = date.getMonth();
-    month+=1;
+    month += 1;
     let day = date.getDate();
 
-    hours = formatTime(hours);
-    minutes = formatTime(minutes);
-    seconds = formatTime(seconds);
-
-    document.getElementById('time').innerHTML = "&nbsp;&nbsp;BPM发波台:&nbsp;" + hours + ":" + minutes + ":" + seconds;
+    document.getElementById('time').innerHTML = "&nbsp;&nbsp;UTC:&nbsp;" + hours + ":" + minutes + ":" + seconds;
     document.getElementById('dating').innerHTML = "&nbsp;&nbsp;" + year + "年" + month + "月" + day + "日";
 }
-
-function formatTime(time) {
-    if (time < 10) {
-        time = "0" + time;
-    }
-    return time;
-}
+/*
+* */
 
 setInterval(showTime, 1000)
